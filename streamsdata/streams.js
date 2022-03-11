02b160634add8e765a51fe0fdd4f6d25f6003c93
DAO NGOC <ngocdb@acbs.com.vn>
2022-03-11 14:12:17 +0700

    Add more logic

--- streamsdata/streams.js
+++ streamsdata/streams.js
@@ -3,7 +3,17 @@ const fs = require('fs');
 //Read stream
 const readStream = fs.createReadStream('./docs/blog3.txt', {highWaterMark: 2048, encoding: "utf-8"});
 //Write stream
+if(fs.existsSync('./docs/blog4.txt')) {
+    fs.unlink('./docs/blog4.txt', (err) => {
+        if(err) {
+            console.log(err);
+        } else {
+            console.log("Done delete file blog4");
+        }
+    })
+}
 const writeStream = fs.createWriteStream('./docs/blog4.txt');
+console.log("Done created blog4 file");
 
 // readStream.on('data', chunk => {
 //     console.log('-----NEW CHUNK----------');
